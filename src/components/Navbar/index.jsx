71aa2b214a48de66934086a50dc4ea28fa04f4ee
impragyaverma <pragya9428@gmail.com>
2024-07-15 17:20:18 +0530

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='h-20 flex items-center w-full text-white'>
-        <div className='text-3xl pl-20 font-bold'>Joboard.</div>
-
+    <div className='h-20 flex items-center w-full bg-blue-800 text-white'>
+      <div className='text-3xl pl-20 font-bold'>Joboard.</div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
